3b3107eae3332f59aba240604acb047d8f5ee597
Sharath Chandra S <sharath.suroj@gmail.com>
2022-03-24 15:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,12 @@ def predict():
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
     
-    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-       'mean smoothness', 'mean compactness', 'mean concavity',
-       'mean concave points', 'mean symmetry', 'mean fractal dimension',
-       'radius error', 'texture error', 'perimeter error', 'area error',
-       'smoothness error', 'compactness error', 'concavity error',
-       'concave points error', 'symmetry error', 'fractal dimension error',
-       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-       'worst smoothness', 'worst compactness', 'worst concavity',
-       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    features_name = [ 'mean concavity',
+       'mean concave points', 'area error',
+        'compactness error', 'concavity error',
+       'worst texture',  'worst area',
+       'worst smoothness', 
+       'worst concave points', 'worst symmetry']
     
     df = pd.DataFrame(features_value, columns=features_name)
     output = model.predict(df)
