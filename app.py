293558f56c7d87028415365588770c3861464a04
Sharath Chandra S <sharath.suroj@gmail.com>
2022-11-12 10:45:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
     return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=0.0.0.0,debug=True,port=5000)
